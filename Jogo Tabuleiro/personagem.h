61766c883822bb63ff4a8194e781a740e48f4b50
afiabarbosa <107432552+Afia-barbosa@users.noreply.github.com>
2025-02-08 21:52:34 -0300

    Delete Jogo Tabuleiro/personagem.h

--- Jogo Tabuleiro/personagem.h
+++
@@ -1,9 +0,0 @@
-#ifndef PERSONAGEM_H
-#define PERSONAGEM_H
-#include <stdbool.h>
-
-void limparposicaoPersonagem(char **tabuleiro, int x, int y);
-void moverPersonagem(char **tabuleiro, int *x, int *y, char tecla, int quantidade_linha, int quantidade_coluna);
-
-#endif
-
